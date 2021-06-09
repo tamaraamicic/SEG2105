9f6d563aadda398f72c1bac90165b102e39e0f24
hisra015 <75385352+hisra015@users.noreply.github.com>
2021-06-09 13:36:32 -0400

    Initialized buttons in MainActivity.java, Created a new User class.

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/MainActivity.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+    Button registerButton;
+    EditText userNameEditText;
+    EditText passwordEditText;
 
 
 
@@ -16,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginButton = (Button)(findViewById(R.id.signInButton));
+        registerButton = (Button)(findViewById(R.id.registerButton));
+        userNameEditText = (EditText) (findViewById(R.id.username));
+        passwordEditText = (EditText) (findViewById(R.id.password));
+
+        User user = new User("admin","admin123");
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(!userNameEditText.getText().toString().equals("") && !passwordEditText.getText().toString().equals("")){
+                    VerifyLogin verifyLogin = new VerifyLogin(userNameEditText.getText().toString(),passwordEditText.getText().toString());
+                }
+            }
+        });
 
 
 
---
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/VerifyLogin.java
@@ -0,0 +1,13 @@
+package com.example.seg2105_term_project;
+
+public class VerifyLogin {
+
+    String user;
+    String password;
+
+    public VerifyLogin(String user,String password){
+        this.user = user;
+        this.password = password;
+    }
+
+}
