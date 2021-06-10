c0b12b4011a58b10bb09bc9b28e1e903649a604f
tamaraamicic <78327636+tamaraamicic@users.noreply.github.com>
2021-06-10 15:59:38 -0400

    CreateCourse class edited

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/CreateCourse.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/CreateCourse.java
@@ -3,6 +3,7 @@ package com.example.seg2105_term_project;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,11 +22,23 @@ public class CreateCourse extends AppCompatActivity {
         inputNameForCreate = (TextInputEditText) (findViewById(R.id.inputNameForCreate));
         inputCodeForCreate = (TextInputEditText) (findViewById(R.id.inputCodeForCreate));
         createCourseButton2 = (Button)(findViewById(R.id.createCourseButton2));
+        MyDBHandlerCourses handlerCourses = new MyDBHandlerCourses(this);
 
         createCourseButton2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if (!inputNameForCreate.getText().toString().equals("") && !inputCodeForCreate.getText().toString().equals("")) {
-
+                String name = inputNameForCreate.getText().toString();
+                String code = inputCodeForCreate.getText().toString();
+                if (!name.equals("") && !code.equals("")) {
+                    if (handlerCourses.findCourse(name, code).equals(null)) {
+                        Course course = new Course(name, code);
+                        handlerCourses.addCourse(course);
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "COURSE WITH SAME CODE ALREADY EXISTS", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "PLEASE INPUT THE NAME AND CODE OF THE COURSE YOU WISH TO ADD", Toast.LENGTH_LONG);
+                    toast.show();
                 }
             }
         });
