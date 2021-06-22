f159ef94d72fa537b21b814477e822d425a074b5
hisra015 <75385352+hisra015@users.noreply.github.com>
2021-06-22 17:24:25 -0400

    Created view_courses.xml, create ViewCoursesPage.java, Now have ability for instructor to see list of courses

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorPage.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorPage.java
@@ -1,16 +1,29 @@
 package com.example.seg2105_term_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InstructorPage extends AppCompatActivity {
-
+    Button viewCoursesButton;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intructor_page);
+
+        viewCoursesButton = (Button)findViewById(R.id.viewCoursesButton);
+
+        viewCoursesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ViewCoursesPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/ViewCoursesPage.java
@@ -0,0 +1,35 @@
+package com.example.seg2105_term_project;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewCoursesPage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_courses);
+
+        MyDBHandlerCourses myDBHandlerCourses = new MyDBHandlerCourses(this);
+
+        TextView entireView = (TextView) findViewById(R.id.entireView);
+
+        Cursor cursor = myDBHandlerCourses.viewData();
+
+        StringBuilder stringBuilder = new StringBuilder("No Data To Show");
+        while (cursor.moveToNext()) {
+            int index = stringBuilder.indexOf("No Data To Show");
+            if (index!=-1){
+                stringBuilder.delete(0,"No Data To Show".length());
+            }
+            stringBuilder.append("\nCourse Name: " + cursor.getString(1) + "\nCourse Code: " + cursor.getString(2));
+            stringBuilder.append("\n");
+        }
+
+
+        entireView.setText(stringBuilder);
+    }
+}
