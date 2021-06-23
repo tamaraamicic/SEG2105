76fe40f22b4800e9c805cb9dbda0b77845b81b4c
hisra015 <75385352+hisra015@users.noreply.github.com>
2021-06-23 14:47:11 -0400

    Created a Time Picker

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/AssignCourse.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/AssignCourse.java
@@ -1,12 +1,48 @@
 package com.example.seg2105_term_project;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class AssignCourse extends AppCompatActivity {
+
+    int hour1,minute1;
+    EditText TimeInput1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assign_course);
+
+        TimeInput1 = findViewById(R.id.TimeInput1);
+
+        TimeInput1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popTimePicker(v);
+            }
+        });
+    }
+
+    public void popTimePicker(View view){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                hour1 = hourOfDay;
+                minute1 = minute;
+                TimeInput1.setText(String.format(Locale.getDefault(), "%02d:%02d",hour1,minute1));
+            }
+        };
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,onTimeSetListener,hour1,minute1,false);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 }
