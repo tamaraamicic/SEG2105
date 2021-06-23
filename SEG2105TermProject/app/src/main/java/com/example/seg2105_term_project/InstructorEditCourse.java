461fd311ed8dede9f5d52bd859ff90595d77e6a4
tamaraamicic <78327636+tamaraamicic@users.noreply.github.com>
2021-06-23 15:49:28 -0400

    edit courses xml

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorEditCourse.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorEditCourse.java
@@ -1,6 +1,27 @@
 package com.example.seg2105_term_project;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class InstructorEditCourse extends AppCompatActivity {
+
+    TextInputEditText inputNameForInstructorEditCourse;
+    TextInputEditText inputCodeForInstructorEditCourse;
+    Button searchButtonForInstructorEditCourse;
+    Button editButtonForInstructorEditCourse;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructor_assign_course);
+
+        inputNameForInstructorEditCourse = (TextInputEditText) (findViewById(R.id.inputNameForInstructorEditCourse));
+        inputCodeForInstructorEditCourse = (TextInputEditText) (findViewById(R.id.inputCodeForInstructorEditCourse));
+        searchButtonForInstructorEditCourse = (Button) (findViewById(R.id.searchButtonForInstructorEditCourse));
+        editButtonForInstructorEditCourse = (Button) (findViewById(R.id.editButtonForInstructorEditCourse));
+        editButtonForInstructorEditCourse.setEnabled(false);
+    }
 }
