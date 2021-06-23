83ec5cfde53f8a1d4f774da3cba435ec1d793b82
hisra015 <75385352+hisra015@users.noreply.github.com>
2021-06-23 15:21:57 -0400

    Instructor can now select date and time from GUI instead of manually entering. Changes in InstructorAssignCoursePart2.java

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorAssignCoursePart2.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorAssignCoursePart2.java
@@ -1,37 +1,69 @@
 package com.example.seg2105_term_project;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 public class InstructorAssignCoursePart2 extends AppCompatActivity {
 
-    int hour1,minute1;
-    EditText TimeInput1;
+    int hour1,minute1,hour2,minute2;
 
+    EditText TimeInput1;
+    EditText TimeInput2;
+    EditText DayInput1;
+    EditText DayInput2;
+    DatePickerDialog datePickerDialog;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instructor_assign_course_part2);
 
         TimeInput1 = findViewById(R.id.TimeInput1);
+        TimeInput2 = findViewById(R.id.TimeInput2);
+        DayInput1 = findViewById(R.id.DayInput1);
+        DayInput2 = findViewById(R.id.DayInput2);
 
         TimeInput1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                popTimePicker(v);
+                Time1Picker(v);
+            }
+        });
+
+        TimeInput2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Time2Picker(v);
+            }
+        });
+
+        DayInput1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InitDatePicker1(v);
+            }
+        });
+
+        DayInput2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InitDatePicker1(v);
             }
         });
     }
 
-    public void popTimePicker(View view){
+    public void Time1Picker(View view){
         TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -45,4 +77,108 @@ public class InstructorAssignCoursePart2 extends AppCompatActivity {
         timePickerDialog.setTitle("Select Time");
         timePickerDialog.show();
     }
+    public void Time2Picker(View view){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                hour2 = hourOfDay;
+                minute2 = minute;
+                TimeInput2.setText(String.format(Locale.getDefault(), "%02d:%02d",hour2,minute2));
+            }
+        };
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,onTimeSetListener,hour2,minute2,false);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
+    public void InitDatePicker1(View view){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month +1;
+                String date =  makeDateString(dayOfMonth, month, year);
+                DayInput1.setText(date);
+
+            }
+        };
+
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int date = cal.get(Calendar.DATE);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,date);
+        datePickerDialog.setTitle("Select Date");
+        datePickerDialog.show();
+    }
+    public void InitDatePicker2(View view){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month +1;
+                String date =  makeDateString(dayOfMonth, month, year);
+                DayInput2.setText(date);
+
+            }
+        };
+
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int date = cal.get(Calendar.DATE);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,date);
+        datePickerDialog.setTitle("Select Date");
+        datePickerDialog.show();
+    }
+
+    public String makeDateString(int day, int month, int year){
+        return getMonthFormat(month)+" "+day+" "+year;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month==1){
+            return "JAN";
+        }
+        if(month==2){
+            return "FEB";
+        }
+        if(month==3){
+            return "MAR";
+        }
+        if(month==4){
+            return "APR";
+        }
+        if(month==5){
+            return "MAY";
+        }
+        if(month==6){
+            return "JUN";
+        }
+        if(month==7){
+            return "JUL";
+        }
+        if(month==8){
+            return "AUG";
+        }
+        if(month==9){
+            return "SEP";
+        }
+        if(month==10){
+            return "OCT";
+        }
+        if(month==11){
+            return "NOV";
+        }
+        if(month==12){
+            return "DEC";
+        }
+        return "JAN";
+    }
+
 }
