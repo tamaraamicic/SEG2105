3b3bcc7932cf6cd7afd391e9a5d4dbe6af8e28d5
tamaraamicic <78327636+tamaraamicic@users.noreply.github.com>
2021-06-24 13:34:09 -0400

    InstructorEditCourse edited

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorEditCourse.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/InstructorEditCourse.java
@@ -1,7 +1,9 @@
 package com.example.seg2105_term_project;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,5 +25,39 @@ public class InstructorEditCourse extends AppCompatActivity {
         searchButtonForInstructorEditCourse = (Button) (findViewById(R.id.searchButtonForInstructorEditCourse));
         editButtonForInstructorEditCourse = (Button) (findViewById(R.id.editButtonForInstructorEditCourse));
         editButtonForInstructorEditCourse.setEnabled(false);
+
+
+        MyDBHandlerCourses handlerCourses = new MyDBHandlerCourses(this);
+
+        searchButtonForInstructorEditCourse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String name = inputNameForInstructorEditCourse.getText().toString();
+                String code = inputCodeForInstructorEditCourse.getText().toString();
+                Course course = handlerCourses.findCourse(name, code);
+                if (course != null) {
+                    if (code.equals("")) {
+                        inputCodeForInstructorEditCourse.setText(course.getCourseCode());
+                    } else if (name.equals("")) {
+                        inputNameForInstructorEditCourse.setText(course.getCourseName());
+                    }
+                    if (course.getInstructor() == null) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "You do not teach this course. Please retry", Toast.LENGTH_LONG);
+                        toast.show();
+                    } else {
+                        if (course.getInstructor().equals(CurrentUser.getUsername())) {
+                            editButtonForInstructorEditCourse.setEnabled(true);
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "You do not teach this course. Please retry", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Course not found. Please retry.", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
     }
 }
