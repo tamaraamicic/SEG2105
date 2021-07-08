ef6a3c9131a25b71c57da9173195f9939cff3271
hisra015 <75385352+hisra015@users.noreply.github.com>
2021-07-08 13:31:34 -0400

    Fixed similarity between StudentViewCourses.java and StudentViewEnrolledCourses.java

--- SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/StudentViewCourses.java
+++ SEG2105TermProject/app/src/main/java/com/example/seg2105_term_project/StudentViewCourses.java
@@ -23,11 +23,9 @@ public class StudentViewCourses extends AppCompatActivity {
             if (index!=-1){
                 stringBuilder.delete(0,"No Data To Show".length());
             }
-            Course course = new Course(cursor.getString(1),cursor.getString(2));
-            if(course.hasStudent(CurrentUser.getUsername())){
-                stringBuilder.append("\nCourse Name: " + cursor.getString(1) + "\nCourse Code: " + cursor.getString(2)+ "\nInstructor: " + cursor.getString(3)+ "\nDate 1: " + cursor.getString(4)+ "\nTime 1: " + cursor.getString(6)+ "\nDate 2: " + cursor.getString(5)+ "\nTime 2: " + cursor.getString(7)+ "\nCapacity: " + cursor.getString(8)+ "\nDescription: " + cursor.getString(9));
-                stringBuilder.append("\n");
-            }
+            stringBuilder.append("\nCourse Name: " + cursor.getString(1) + "\nCourse Code: " + cursor.getString(2)+ "\nInstructor: " + cursor.getString(3)+ "\nDate 1: " + cursor.getString(4)+ "\nTime 1: " + cursor.getString(6)+ "\nDate 2: " + cursor.getString(5)+ "\nTime 2: " + cursor.getString(7)+ "\nCapacity: " + cursor.getString(8)+ "\nDescription: " + cursor.getString(9));
+            stringBuilder.append("\n");
+
 
         }
 
